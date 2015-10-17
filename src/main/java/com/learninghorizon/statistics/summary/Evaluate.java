76742da62af9b41f4987e7b4ebfcc24113615196
BHARAT SAVANI <savanibharat@gmail.com>
2015-10-17 00:11:17 -0700

    Marker Interface Evaluate

---
+++ src/main/java/com/learninghorizon/statistics/summary/Evaluate.java
@@ -0,0 +1,9 @@
+package com.learninghorizon.statistics.summary;
+
+/**
+ * Evaluate is an interface.
+ * See {@link Sum}, {@link Mean}
+ * */
+public interface Evaluate extends UniEvaluate, BiEvaluate, MultiEvaluate{
+	
+}
