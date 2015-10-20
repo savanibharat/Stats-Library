e03855ca78f2776d03f8a40e0118a82b1c1f572e
BHARAT SAVANI <savanibharat@gmail.com>
2015-10-19 20:14:41 -0700

    Mean interface important comments added.

--- src/main/java/com/learninghorizon/statistics/summary/Mean.java
+++ src/main/java/com/learninghorizon/statistics/summary/Mean.java
@@ -1,8 +1,17 @@
 package com.learninghorizon.statistics.summary;
 
 /**
- * Mean interface 
+ * Mean interface
+ * This interface is used to evaluate all different
+ * types of Mean.
+ * Arithmetic mean (AM) 
+ * 			https://en.wikipedia.org/wiki/Arithmetic_mean
+ * Geometric mean (GM) 
+ * 			https://en.wikipedia.org/wiki/Geometric_mean
+ * Harmonic mean (HM) 
+ * 			https://en.wikipedia.org/wiki/Harmonic_mean
+ * : Inequality of arithmetic and geometric means 
+ * 			https://en.wikipedia.org/wiki/Inequality_of_arithmetic_and_geometric_means
  * */
 public interface Mean extends Evaluate{
-
 }
